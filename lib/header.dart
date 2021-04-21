b4c75e5fa34073efe2ee0071f52064a44f70067f
enocturne <enocturne@126.com>
2021-04-22 00:57:06 +0800

    数据处理函数，暂时没有处理非法数据，将MapData中的点集改为划分校区的形式

--- lib/header.dart
+++ lib/header.dart
@@ -1,5 +1,6 @@
 import 'package:amap_flutter_base/amap_flutter_base.dart'; //LatLng 类型在这里面
 import 'dart:math';
+import 'dart:io';
 
 //建筑类定义
 class Building {
@@ -19,7 +20,7 @@ class Edge {
   int availmthod = 1;
   //边拥挤度，需要时调用随机方法生成。
   double crowding = 1;
-//构造函数
+  //构造函数
   Edge(LatLng pointa, LatLng pointb, {int availmthod = 1}) {
     var p = 0.017453292519943295;
     var c = cos;
@@ -54,7 +55,135 @@ class BusTimeTable {
 class MapData {
   Map<int, String> mapcampus;
   List<Building> mapbuilding;
-  Map<int, LatLng> mapvertex;
+  List<Map<int, LatLng>> mapvertex;
   List<List<List<Edge>>> mapedge;
   List<BusTimeTable> mapbustimetable;
+}
+
+MapData dataInput(String path) {
+  final inputfile = File(path);
+  List<String> lines = inputfile.readAsLinesSync();
+  MapData inputData = new MapData();
+  inputData.mapcampus = campusInput(lines[0]);
+  inputData.mapvertex = pointsInput(lines[1]);
+  inputData.mapedge = null;
+  inputData.mapbustimetable = bustableInput(lines[3]);
+  List<String> tmp_lines = [lines[4], lines[5], lines[6]];
+  inputData.mapbuilding = buildingInput(tmp_lines);
+}
+
+//校区数据导入
+Map<int, String> campusInput(String line) {
+  List<String> name = line.split(',');
+  List<int> number = List.generate(name.length, (index) => index);
+  Map<int, String> campus_map = Map.fromIterables(number, name);
+  return campus_map;
+}
+
+//点集导入
+List<Map<int, LatLng>> pointsInput(String line) {
+  List<String> tmp_str = line.split(';');
+  List<Map<int, LatLng>> points_list = [];
+  for (int i = 0; i < tmp_str.length; i++) {
+    List<String> points = tmp_str[i].split(',');
+    List<LatLng> latlngs_list = [];
+    for (int j = 0; j < points.length / 2; j++) {
+      LatLng tmp = new LatLng(
+          double.parse((points[j * 2])), double.parse((points[j * 2 + 1])));
+      latlngs_list.add(tmp);
+    }
+    List<int> number = List.generate(latlngs_list.length, (index) => index);
+    Map<int, LatLng> latlngs_map = Map.fromIterables(number, latlngs_list);
+    points_list.add(latlngs_map);
+  }
+  return points_list;
+}
+
+//边集导入
+List<List<Edge>> edgesInput(String line, Map<int, LatLng> latlngs_map) {
+  List<String> tmp_str = line.split(';');
+  List<List<Edge>> edges = [];
+  for (int i = 0; i < tmp_str.length; i++) {
+    List<String> edges_str = tmp_str[i].split(',');
+    List<Edge> edges_list = [];
+    LatLng xpoint = new LatLng(-1, -1);
+    for (int j = 0; j < edges_str.length ~/ 2; j++) {
+      LatLng point1 = latlngs_map[int.parse(edges_str[j * 2])] ?? xpoint;
+      LatLng point2 = latlngs_map[int.parse(edges_str[j * 2 + 1])] ?? xpoint;
+
+      if (point1 == xpoint || point2 == xpoint) {
+        //throw an exception
+      }
+      Edge tmp = new Edge(point1, point2);
+      edges_list.add(tmp);
+    }
+    edges.add(edges_list);
+  }
+  return edges;
+}
+
+//校车数据导入
+
+List<BusTimeTable> bustableInput(String line) {
+  List<String> bustable_str = line.split(',');
+  List<BusTimeTable> bustable_list = [];
+  for (int i = 0; i < bustable_str.length ~/ 3; i++) {
+    BusTimeTable tmp = new BusTimeTable();
+    tmp.campusfrom = int.parse(bustable_str[i * 3]);
+    //*TODO exception
+    tmp.campusto = int.parse(bustable_str[i * 3 + 1]);
+    int timeinfo = int.parse(bustable_str[i * 3 + 2]);
+    tmp.dayofweek = timeinfo ~/ 10000;
+    //*TODO exception
+    tmp.setoutminute = timeinfo % 100;
+    tmp.setouthour = timeinfo ~/ 100 % 100;
+    bustable_list.add(tmp);
+  }
+  return bustable_list;
+}
+
+//建筑集导入
+List<Building> buildingInput(List<String> line) {
+  List<String> descrip_str = line[0].split(';');
+  List<List<String>> descrip_list = [];
+  for (int i = 0; i < descrip_str.length; i++) {
+    List<String> tmp_str = descrip_str[i].split(',');
+    List<String> tmp_list = [];
+    for (int j = 0; j < tmp_str.length; j++) {
+      tmp_list.add(tmp_str[j]);
+    }
+    descrip_list.add(tmp_list);
+  }
+  //print(descrip_list);
+  List<String> entry_str = line[1].split(';');
+  List<List<int>> entry_list = [];
+  for (int i = 0; i < entry_str.length; i++) {
+    List<String> tmp_str = entry_str[i].split(',');
+    List<int> tmp_list = [];
+    for (int j = 0; j < tmp_str[j].length; j++) {
+      tmp_list.add(int.parse(tmp_str[j]));
+    }
+    entry_list.add(tmp_list);
+  }
+
+  //print(entry_list);
+
+  List<String> number_str = line[2].split(',');
+  List<int> number_list = [];
+  for (int i = 0; i < number_str.length; i++) {
+    number_list.add(int.parse(number_str[i]));
+  }
+
+  //print(number_list);
+  List<Building> build_list = [];
+  for (int i = 0; i < descrip_str.length; i++) {
+    Building tmp = new Building();
+    tmp.doors = entry_list[i];
+    tmp.description = descrip_list[i];
+    tmp.incampus = number_list[i];
+
+    build_list.add(tmp);
+  }
+
+  return build_list;
 }
