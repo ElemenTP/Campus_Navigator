b25b703c55cdef40685450251e933c048276aa95
ElemenTP <fpjraidden@outlook.com>
2021-05-11 00:36:16 +0800

    迁移至null-safety，放弃fluttertoast，使用dialog提醒用户

--- lib/settingpage.dart
+++ lib/settingpage.dart
@@ -1,42 +1,44 @@
 import 'package:amap_flutter_map/amap_flutter_map.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 
 class MySettingPage extends StatefulWidget {
-  MySettingPage({Key key, @required this.mapController}) : super(key: key);
+  MySettingPage({Key key = const Key('setting'), @required this.mapController})
+      : super(key: key);
 
-  final AMapController mapController;
+  final AMapController? mapController;
 
   @override
   State<StatefulWidget> createState() => _MySettingPageState();
 }
 
 class _MySettingPageState extends State<MySettingPage> {
-  //普通地图审图号
-  String _mapContentApprovalNumber;
   //卫星地图审图号
-  String _satelliteImageApprovalNumber;
+  String _satelliteImageApprovalNumber = '地图未正常加载';
 
   //获取审图号函数
   void _getApprovalNumber() async {
-    //按要求获取普通地图审图号
-    _mapContentApprovalNumber =
-        await widget.mapController?.getMapContentApprovalNumber();
     //按要求获取卫星地图审图号
-    _satelliteImageApprovalNumber =
-        await widget.mapController?.getSatelliteImageApprovalNumber();
+    await widget.mapController?.getSatelliteImageApprovalNumber().then((value) {
+      if (value != null) _satelliteImageApprovalNumber = value;
+    });
     setState(() {});
   }
 
   //清除地图缓存函数
   void _cleanMapCache() async {
     await widget.mapController?.clearDisk();
-    Fluttertoast.showToast(
-      msg: '已清除缓存',
-      backgroundColor: Colors.black,
-      textColor: Colors.white,
-      fontSize: 16,
-    );
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("提示"),
+              content: Text("地图缓存已清除。"),
+              actions: <Widget>[
+                TextButton(
+                  child: Text("确定"),
+                  onPressed: () => Navigator.of(context).pop(), //关闭对话框
+                ),
+              ],
+            ));
   }
 
   @override
@@ -67,10 +69,6 @@ class _MySettingPageState extends State<MySettingPage> {
                   onPressed: _cleanMapCache,
                 )
               ],
-            ),
-            Text(
-              '普通地图审图号：$_mapContentApprovalNumber',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             Text(
               '卫星地图审图号：$_satelliteImageApprovalNumber',
